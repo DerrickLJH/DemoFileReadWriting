cbdb1fb2c2deea9dec78f9f9b174761ca68da2ec
17010457 <17010457@myrp.edu.sg>
2019-07-09 14:21:50 +0800

    Done

--- app/src/main/java/com/myapplicationdev/android/demofilereadwriting/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demofilereadwriting/MainActivity.java
@@ -10,14 +10,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class MainActivity extends AppCompatActivity {
 
     String folderLocation;
     //UI handlers to be defined
     Button btnWrite, btnRead;
+    TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +33,10 @@ public class MainActivity extends AppCompatActivity {
         //UI handlers to be defined
         btnWrite = findViewById(R.id.btnWrite);
         btnRead = findViewById(R.id.btnRead);
-
+        tvText = findViewById(R.id.tvText);
         if (checkPermission()) {
 
-            folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
+            folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/P09";
 
             File folder = new File(folderLocation);
             if (folder.exists() == false) {
@@ -44,7 +50,15 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     //Code for file writing
-
+                    try {
+                        File file = new File(folderLocation, "file.txt");
+                        FileWriter fileWriter = new FileWriter(file, true);
+                        fileWriter.write("Hello world!\n");
+                        fileWriter.flush();
+                        fileWriter.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             });
 
@@ -52,6 +66,30 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     //Code for file reading
+                    File file = new File(folderLocation, "file.txt");
+                    if (file.exists() == true){
+                        String data = "";
+                        try{
+                            FileReader reader = new FileReader(file);
+                            BufferedReader br = new BufferedReader(reader);
+
+                            String line = br.readLine();
+                            while (line!=null){
+                                data += line + "\n";
+                                line = br.readLine();
+                            }
+
+                            br.close();
+                            reader.close();
+                        }catch (Exception e){
+                            Toast.makeText(MainActivity.this,"Failed to read file!", Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+                        Log.d("Content", data);
+                        tvText.setText(data);
+                    } else {
+                        Toast.makeText(MainActivity.this, "File not found!", Toast.LENGTH_SHORT).show();
+                    }
                 }
             });
         } else {
